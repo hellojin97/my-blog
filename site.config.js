6a85962aa0759b1c0df41d4694e0e3d6e38e7ea2
김현진 <hjkim@gimhyeonjin-ui-noteubug.local>
2024-04-14 04:41:02 +0900

    update projects

--- site.config.js
+++ site.config.js
@@ -11,10 +11,8 @@ const CONFIG = {
     instagram: "",
   },
   projects: [
-    {
       // name: `morethan-log`,
       // href: "https://github.com/morethanmin/morethan-log",
-    },
   ],
   // blog setting (required)
   blog: {
